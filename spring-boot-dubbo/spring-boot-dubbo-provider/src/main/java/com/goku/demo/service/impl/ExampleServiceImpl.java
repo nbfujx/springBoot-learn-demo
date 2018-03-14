33ba0ff859271b03fd6bac8cd74a0a96e9d750d0
nbfujx <nbfujx@qq.com>
2018-03-14 21:19:06 +0800

    新增activemq

--- spring-boot-dubbo/spring-boot-dubbo-provider/src/main/java/com/goku/demo/service/impl/ExampleServiceImpl.java
+++ spring-boot-dubbo/spring-boot-dubbo-provider/src/main/java/com/goku/demo/service/impl/ExampleServiceImpl.java
@@ -6,7 +6,7 @@ import com.alibaba.dubbo.config.annotation.Service;
 /**
  * Created by nbfujx on 2017-11-23.
  */
-@Service(version = "1.0.0")
+@Service(version = "1.0.0",retries=9,timeout =3000,loadbalance="leastactive")
 public class ExampleServiceImpl implements ExampleService {
 
     @Override
