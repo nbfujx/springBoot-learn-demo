d72f9cbde5eb40e859407d29594fbf07dceb8f73
nbfujx <nbfujxx@qq.com>
2018-05-03 22:36:10 +0800

    添加kafka项目

---
+++ spring-boot-kafka/spring-boot-kafka-receiver/src/main/java/com/goku/receiver/KafkaReceiver.java
@@ -0,0 +1,14 @@
+package com.goku.receiver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author nbfujx
+ * @date 2018/5/3 21:46
+ */
+@Component
+@Slf4j
+public class KafkaReceiver{
+
+}
---
+++ spring-boot-kafka/spring-boot-kafka-sender/src/main/java/com/goku/sender/KafkaSender.java
@@ -0,0 +1,14 @@
+package com.goku.sender;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author nbfujx
+ * @date 2018/5/3 21:44
+ */
+@Component
+@Slf4j
+public class KafkaSender{
+
+}
